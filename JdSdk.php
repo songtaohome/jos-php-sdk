17cb193b38430be5b6fdf04bde385e28b3540702
MinSongTao <minsongtao@wangdian.cn>
2022-01-13 10:49:00 +0800

    泰国

---
+++ JdSdk.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * JD SDK 入口文件
+ * 请不要修改这个文件，除非你知道怎样修改以及怎样恢复
+ */
+
+/**
+ * 定义常量开始
+ * 在include("JdSdk.php")之前定义这些常量，不要直接修改本文件，以利于升级覆盖
+ */
+/**
+ * SDK工作目录
+ * 存放日志，JD缓存数据
+ */
+if (!defined("JD_SDK_WORK_DIR"))
+{
+	define("JD_SDK_WORK_DIR", "/tmp/");
+}
+/**
+ * 是否处于开发模式
+ * 在你自己电脑上开发程序的时候千万不要设为false，以免缓存造成你的代码修改了不生效
+ * 部署到生产环境正式运营后，如果性能压力大，可以把此常量设定为false，能提高运行速度（对应的代价就是你下次升级程序时要清一下缓存）
+ */
+if (!defined("JD_SDK_DEV_MODE"))
+{
+	define("JD_SDK_DEV_MODE", true);
+}
+/**
+ * 定义常量结束
+ */
+
+/**
+ * 找到lotusphp入口文件，并初始化lotusphp
+ * lotusphp是一个第三方php框架，其主页在：lotusphp.googlecode.com
+ */
+$lotusHome = dirname(__FILE__) . DIRECTORY_SEPARATOR . "lotusphp_runtime" . DIRECTORY_SEPARATOR;
+include($lotusHome . "Lotus.php");
+$lotus = new Lotus;
+$lotus->option["autoload_dir"] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'jd';
+
+$lotus->devMode = JD_SDK_DEV_MODE;
+$lotus->defaultStoreDir = JD_SDK_WORK_DIR;
+
+$lotus->init();
+
+$pt = "my cipher";
+$accessToken = "596f309be35643c88abefce9799e2ac2owjl";
+$appKey = "39B4F855DA79FB68B53CCED349362BA8";
+$appSecret = "6594847ca79a44a9884c88ce91a2b1fb";
+$tde = SecretJdClient::getInstance($accessToken, $appKey, $appSecret);
+
+$ct = $tde->encrypt($pt);
+
+$tde = SecretJdClient::getInstance($accessToken, $appKey);
+
+$decrypted = $tde->decrypt($ct);
+print_r($decrypted);
+die;
